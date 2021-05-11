6f9972c7cb40b408edf7a1a808675f50486f4f1c
StephenDunne97 <dunne134@gmail.com>
2021-05-11 14:16:40 +0100

    Conditional Application of CSS classes.
    Using CSS Class binding to bind a styling class.

--- build-a-bot/src/builder/RobotBuilder.vue
+++ build-a-bot/src/builder/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
         <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
         <div class="top-row">
-            <div class="top part" :style="headBorderStyle">
+            <div :class="[saleBorderClass, 'top', 'part']">
                 <div class="robot-name">
                   {{selectedRobot.head.title}}
                   <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -84,6 +84,9 @@ export default {
   },
 
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
     headBorderStyle() {
       return{
         border: this.selectedRobot.head.onSale ?
@@ -291,6 +294,9 @@ td, th {
 }
 .cost {
   text-align: right;
+}
+.sale-border {
+  border: 3px solid red;
 }
 
 </style>
