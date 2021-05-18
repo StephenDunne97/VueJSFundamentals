588e1e0cb4bbda08ae388936b9c05519ff6efc55
StephenDunne97 <dunne134@gmail.com>
2021-05-18 14:52:12 +0100

    Nested Routing
    Used 'Children' in routes index.js to route between the various parts available.
    
    Added Child components for each part. (Head, Arms, Tosos, Bases)

--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../builder/RobotBuilder.vue';
 import PartsInfo from '../parts/PartsInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
 
 const routes = [ // Array of routes
   {
@@ -13,6 +18,32 @@ const routes = [ // Array of routes
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  },
+  {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      }],
   },
   {
     path: '/parts/:partType/:id',
